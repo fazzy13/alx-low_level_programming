a3d0a8fb15e3c65e5ab028a61a3d8844de52413c
fazzy13 <kaluifeanyi003@gmail.com>
2023-03-20 21:37:47 +0100

    program that prints exactly

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
 *main - programm entry point
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	write("and that piece of art is useful\" -Dora Korpa, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" -Dora Korpa, 2015-10-19\n", 59);
 
 	return (1);
 }
