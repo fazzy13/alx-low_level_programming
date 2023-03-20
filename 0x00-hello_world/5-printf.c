f3860f4ab090b09e8e62b6b02e12546dfcf69f1b
fazzy13 <kaluifeanyi003@gmail.com>
2023-03-20 19:04:05 +0100

    program that prints with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: 0 is returned for exit
+*
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
