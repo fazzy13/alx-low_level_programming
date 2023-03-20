53378a207a7d735e945b2ab4c55d4e2b19b28123
fazzy13 <kaluifeanyi003@gmail.com>
2023-03-20 15:18:11 +0100

    program that prints exact characters

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return : Always
+*
+**/
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+
+	return (0);
+}
