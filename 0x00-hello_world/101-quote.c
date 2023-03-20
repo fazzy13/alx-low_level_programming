cd03f347a22730ed830fbbf73cd389c09bd97c40
fazzy13 <kaluifeanyi003@gmail.com>
2023-03-20 21:24:29 +0100

    program that prints exactly

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - programm entry point
+*
+*Return: return 1
+*/
+
+int main(void)
+{
+	write("and that piece of art is useful\" -Dora Korpa, 2015-10-19\n");
+
+	return (1);
+}
