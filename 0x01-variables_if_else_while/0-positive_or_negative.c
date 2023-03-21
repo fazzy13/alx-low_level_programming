9a21842dc7b6a641810517a3ec4889dc2726c506
fazzy13 <kaluifeanyi003@gmail.com>
2023-03-21 15:13:25 +0100

    print random numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("is positive\n");
+		printf("%i is positive\n");
 	}
 	else if (n == 0)
 	{
-		printf("is zero\n");
+		printf("%i is zero\n");
 	}
 	else
 	{
-		printf("is negative\n");
+		printf("%i is negative\n");
 	}
 
 
