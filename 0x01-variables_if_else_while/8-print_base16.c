b73f8b85713339f108198b3862fbb79f89837fa9
fazzy13 <kaluifeanyi003@gmail.com>
2023-03-22 15:49:56 +0100

    prints all the numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point to find base 16
+*
+* Return: exit point after finding base 16
+*
+*/
+
+int main()
+{
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
