f3fcddbb8a0aaad178e17e5fc6255fbff9c54851
fazzy13 <kaluifeanyi003@gmail.com>
2023-03-22 15:06:11 +0100

    prints the lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point of the programm
+*
+* Return: the exit point of the programm
+*
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 26; i >= 0; i--)
+	{
+		putchar(i + 'a');
+	}
+	putchar('\n');
+	return (0);
+}
