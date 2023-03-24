6e3b2cad1c6e45b20a8885013e4cafb855861546
fazzy13 <kaluifeanyi003@gmail.com>
2023-03-25 00:19:47 +0100

    prints the alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_alphabet - Prints the characters in lower
+*
+*Retrun: exit always 0
+*
+*/
+
+void print_alphabet(void)
+{
+	char lower = 97;
+
+	while (lower != 122)
+	{
+		_putchar(lower);
+		lower++;
+	}
+	_putchar('\n');
+}
