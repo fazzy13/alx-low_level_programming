b15083a3901107c47598071a3fd64112f94ad933
fazzy13 <kaluifeanyi003@gmail.com>
2023-03-26 06:52:14 +0100

    computes the absolute value of an integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,8 +3,10 @@
 /**
 * _abs - returns absolute value
 *
-*/ 
+* @num: the integer beign returned
+* Return: returns the absolute value
+*/
 int _abs(int num)
 {
-	return abs(num);
+	return (abs(num));
 }
