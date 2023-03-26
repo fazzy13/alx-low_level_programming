d5e44b5f9ad1369fcaed51e0faffc35de984c9fc
fazzy13 <kaluifeanyi003@gmail.com>
2023-03-26 16:59:53 +0100

    prints every minute starting from 00:00 to 23:59

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* jack_bauer - print every sec of the day
+*
+*/
+void jack_bauer(void)
+{
+	int hours, mins;
+
+	for (hours = 0; hours <= 23; hours++)
+	{
+		for (mins = 0; mins <= 59; mins++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((mins / 10) + '0');
+			_putchar((mins % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
