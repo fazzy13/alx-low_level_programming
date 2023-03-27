d34daebdb88462f253c459aa841c463f715156f3
fazzy13 <kaluifeanyi003@gmail.com>
2023-03-27 02:07:44 +0100

    prints the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,38 +11,38 @@ void print_times_table(int n);
  */
 void print_times_table_row(int n, int multiplier)
 {
-    int result = n * multiplier;
-    int digit1, digit2, digit3;
+	int result = n * multiplier;
+	int digit1, digit2, digit3;
 
-    if (result > 99)
-    {
-        digit1 = result / 100;
-        digit2 = (result / 10) % 10;
-        digit3 = result % 10;
-        _putchar(',');
-        _putchar(' ');
-        _putchar(digit1 + '0');
-        _putchar(digit2 + '0');
-        _putchar(digit3 + '0');
-    }
-    else if (result > 9)
-    {
-        digit1 = result / 10;
-        digit2 = result % 10;
-        _putchar(',');
-        _putchar(' ');
-        _putchar(' ');
-        _putchar(digit1 + '0');
-        _putchar(digit2 + '0');
-    }
-    else
-    {
-        _putchar(',');
-        _putchar(' ');
-        _putchar(' ');
-        _putchar(' ');
-        _putchar(result + '0');
-    }
+	if (result > 99)
+	{
+		digit1 = result / 100;
+		digit2 = (result / 10) % 10;
+		digit3 = result % 10;
+		_putchar(',');
+		_putchar(' ');
+		_putchar(digit1 + '0');
+		_putchar(digit2 + '0');
+		_putchar(digit3 + '0');
+	}
+	else if (result > 9)
+	{
+		digit1 = result / 10;
+		digit2 = result % 10;
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(digit1 + '0');
+		_putchar(digit2 + '0');
+	}
+	else
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(result + '0');
+	}
 }
 
 /**
@@ -54,18 +54,18 @@ void print_times_table(int n)
 {
 	int i, j;
 
-    if (n < 0 || n > 15)
-        return;
+	if (n < 0 || n > 15)
+		return;
 
-    for (i = 0; i <= n; i++)
-    {
-        _putchar('0');
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('0');
 
-        for (j = 1; j <= n; j++)
-        {
-            print_times_table_row(j, i);
-        }
-        _putchar('\n');
-    }
+		for (j = 1; j <= n; j++)
+		{
+			print_times_table_row(j, i);
+		}
+		_putchar('\n');
+	}
 }
 
