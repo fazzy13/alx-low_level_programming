8d012811e13fc2260e8865a3fc91a61ba5fbf148
fazzy13 <kaluifeanyi003@gmail.com>
2023-03-27 20:31:12 +0100

    checks for a digit (0 through 9).

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+
+/**
+* _isdigit - fuction to check super chars
+*
+* @c: character to check
+*
+* Return: exit point of the programm
+*/
+
+int _isdigit(int c)
+{
+	if (_isdigit(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
