321763cb2db5e2f2da44fab342a9015d87972811
fazzy13 <kaluifeanyi003@gmail.com>
2023-03-27 11:32:44 +0100

    prints the sum of the even-valued terms

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main - print even numers of fibonnacci
+*
+* Return: the exit point of the programm
+*/
+
+int main(void)
+{
+	int first_term = 1;
+	int second_term = 2;
+	int sum = 0;
+	int current_term = second_term;
+
+	while (current_term <= 4000000)
+	{
+		if (current_term % 2 == 0)
+		{
+			sum += current_term;
+		}
+		int next_term = first_term + second_term;
+
+		first_term = second_term;
+		second_term = next_term;
+		current_term = second_term;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
