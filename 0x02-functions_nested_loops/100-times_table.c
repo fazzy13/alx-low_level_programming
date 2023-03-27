6c72a1b71c2d60e4f22e7e4994cce9e1c468177a
fazzy13 <kaluifeanyi003@gmail.com>
2023-03-27 01:46:13 +0100

    prints the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,72 @@
 #include "main.h"
+#include <stdio.h>
+
+void print_times_table_row(int n, int multiplier);
+void print_times_table(int n);
 
 /**
- * print_times_table - prints the n times table starting with 0
+ * print_times_table_row - print a row of the n times table
+ * @n: integer argument
+ * @multiplier: integer argument to multiply by
+ *
+ */
+void print_times_table_row(int n, int multiplier)
+{
+    int result = n * multiplier;
+    int digit1, digit2, digit3;
+
+    if (result > 99)
+    {
+        digit1 = result / 100;
+        digit2 = (result / 10) % 10;
+        digit3 = result % 10;
+        putchar(',');
+        putchar(' ');
+        putchar(digit1 + '0');
+        putchar(digit2 + '0');
+        putchar(digit3 + '0');
+    }
+    else if (result > 9)
+    {
+        digit1 = result / 10;
+        digit2 = result % 10;
+        _putchar(',');
+        _putchar(' ');
+        _putchar(' ');
+        _putchar(digit1 + '0');
+        _putchar(digit2 + '0');
+    }
+    else
+    {
+        _putchar(',');
+        _putchar(' ');
+        _putchar(' ');
+        _putchar(' ');
+        _putchar(result + '0');
+    }
+}
+
+/**
+ * print_times_table - print the n times table
  * @n: integer argument
  *
  */
 void print_times_table(int n)
 {
-	int i, j, result;
+    if (n < 0 || n > 15)
+        return;
 
-	if (n > 15 || n < 0)
-		return;
+    int i, j;
 
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
-		{
-			result = i * j;
-			if (j == 0)
-			{
-				_putchar(result + '0');
-			}
-			else if (result < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(result + '0');
-			}
-			else if (result < 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(result / 10 + '0');
-				_putchar(result % 10 + '0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(result / 100 + '0');
-				_putchar((result / 10) % 10 + '0');
-				_putchar(result % 10 + '0');
-			}
-		}
-		_putchar('\n');
-	}
+    for (i = 0; i <= n; i++)
+    {
+        _putchar('0');
+
+        for (j = 1; j <= n; j++)
+        {
+            print_times_table_row(j, i);
+        }
+        _putchar('\n');
+    }
 }
 
