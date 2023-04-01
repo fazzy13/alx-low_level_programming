a2cf9054ffebcb90b63f4dd2eca0eb00bf9a1a31
fazzy13 <kaluifeanyi003@gmail.com>
2023-04-01 21:36:57 +0100

    prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+* puts_half - a function that prints half of a string
+* 
+* @str: the string to divide
+*
+*/
+void puts_half(char *str)
+{
+	int len = strlen(str);
+	int i;
+	int half = len / 2;
+
+	for (i = 0; i < half; i++)
+	{
+		printf("%c", str[i]);
+	}
+	printf("\n");
+}
