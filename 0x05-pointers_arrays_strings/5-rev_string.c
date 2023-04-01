ad619a9a874f0479a813d02bd5c9dd3e40fd278c
fazzy13 <kaluifeanyi003@gmail.com>
2023-04-01 17:12:55 +0100

    that reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,11 +10,12 @@ void rev_string(char *s)
 	char len = strlen(s);
 	int i, j;
 
-	for (i = 0; j = len - 1; i < j; i++ j--)
+	while (i >= 0)
 	{
 		char temp = s[i];
 
 		s[i] = s[j];
 		s[j] = temp;
+		i++;
 	}
 }
