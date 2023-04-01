c05d0dafc89eed0bf02a758ed8a487ea40e19cab
fazzy13 <kaluifeanyi003@gmail.com>
2023-04-01 21:40:57 +0100

    prints half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,7 +3,7 @@
 
 /**
 * puts_half - a function that prints half of a string
-* 
+*
 * @str: the string to divide
 *
 */
@@ -11,7 +11,7 @@ void puts_half(char *str)
 {
 	int len = strlen(str);
 	int i;
-	int half = (len - 1) / 2;
+	int half = len / 2;
 
 	for (i = 0; i < half; i++)
 	{
