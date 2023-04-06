67be54aa8adf04ad72c8aeb18a153325463d9750
fazzy13 <kaluifeanyi003@gmail.com>
2023-04-07 00:43:42 +0100

    generates random valid

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,7 +7,7 @@
 * Return: Always (0)
 *
 */
-
+/**
 #define PASSWORD_LENGTH 10
  int main(void)
 {
@@ -31,3 +31,32 @@
 
 	return (0);
 }
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 10
+
+int main(void)
+{
+    char password[PASSWORD_LENGTH + 1]; // +1 for null terminator
+
+    srand(time(NULL)); // initialize random number generator with current time
+
+    for (int i = 0; i < PASSWORD_LENGTH; i++) {
+        int c;
+        do {
+            c = rand() % 127; // generate random character code between 0 and 126
+        } while (!isalnum(c)); // ensure character is alphanumeric
+        password[i] = c;
+    }
+
+    password[PASSWORD_LENGTH] = '\0'; // null terminator
+
+    printf("Random password: %s\n", password);
+
+    return 0;
+}
+
