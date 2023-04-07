f37ec14d50d6bfb104cd218f8faeb0308f484bbc
fazzy13 <kaluifeanyi003@gmail.com>
2023-04-07 01:52:25 +0100

    generates random valid

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,64 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <ctype.h>
 /**
 * main - a program that generates random valid passwords for the program 101-crackme.
 *
 * Return: Always (0)
 *
 */
-/**
-#define PASSWORD_LENGTH 10
- int main(void)
-{
-	char pass[PASSWORD_LENGTH + 1];
-	int i;
-
-	srand(time(NULL));
-
-	for (i = 0; i < PASSWORD_LENGTH; i++)
-	{
-		int characters;
-
-		while (!isalnum(characters))
-		{
-			characters = rand() % 127;
-			characters++;
-		}
-		pass[i] = characters;
-	}
-	pass[PASSWORD_LENGTH] = '\0';
-
-	return (0);
-}
-*/
-
+i
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 10
-
 int main(void)
 {
-    char password[PASSWORD_LENGTH + 1];
-	int i;
-	int c;
+	int random;
+	int add;
+	int sum;
 
     srand(time(NULL));
 
-    for (i = 0; i < PASSWORD_LENGTH; i++) {
+    for (add = 0; sum = 2772; total > 122; add++)
+	{
 
-        do {
-            c = rand() % 125 + 1;
-        } while (!isalnum(c));
-        password[i] = c;
+		random = rand() % 125 + 1;
+		printf("%c", random);
+		sum -= random;
+        
     }
 
-    password[PASSWORD_LENGTH] = '\0';
-
-    printf("Random password: %s\n", password);
+    printf("%c", sum);
 
     return 0;
 }
