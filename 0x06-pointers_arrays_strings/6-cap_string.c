ea689c7158750320fa0d3c72c60c629f241c296c
fazzy13 <kaluifeanyi003@gmail.com>
2023-04-10 19:32:03 +0100

    capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,18 @@
+/**
+* cap_string - a function that capitalizes all words of a string.
+*
+* @str: the string to convert
+*
+* Return: Return str
+*/
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = str[i] - 32
+	}
+	return (str);
+}
