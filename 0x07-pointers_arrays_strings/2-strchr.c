863134370a303aef4daf2db5010df0c32a72b333
fazzy13 <kaluifeanyi003@gmail.com>
2023-04-13 23:03:56 +0100

    mplement find char function

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,24 +1,21 @@
 /**
- * _strchr - Find a character in a string
- * @s: Pointer to the string to search
+ * _strchr - Locates a character in a string
+ * @s: Pointer to the input string
  * @c: Character to find
  *
- * Return: Pointer to the first occurrence of the character in the string,
- *         or NULL if not found
+ * Return: Pointer to the first occurrence of the character c in the string s,
+ *         or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
 {
-    while (*s != '\0')
-    {
-        if (*s == c)
-        {
-            return ((char *)s);
-        }
-		else if (* != c)
-		{
-			return (NULL);
-		}
-        s++;
-    }
+	while (*s)
+	{
+		if (*s == c)
+			return s;
+		s++;
+	}
+	if (*s == c)
+		return s;
+	return NULL;
 }
 
