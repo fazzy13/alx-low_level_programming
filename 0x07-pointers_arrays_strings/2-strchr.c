5b11992976a849feeb6f72b83e37d15b86a6ddf3
fazzy13 <kaluifeanyi003@gmail.com>
2023-04-13 23:40:29 +0100

    implement find char function

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,15 +10,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	if (s == NULL)
-		return NULL;
-
-	while (*s != '\0' && *s != c)
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		else if (*(s + 1) == c)
+			return (s + 1);
 		s++;
+	}
 
-	if (*s == c)
-		return (s);
-
-	return (NULL);
+	return (s + 1);
 }
-
