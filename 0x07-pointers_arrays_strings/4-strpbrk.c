422aad120059641931b11e739a253bf0f3a0eed7
fazzy13 <kaluifeanyi003@gmail.com>
2023-04-14 03:24:40 +0100

    function to search for byte matches in a string

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,26 +1,35 @@
-#include "main.h"
-
 /**
- * _strpbrk - Searches a string for any of a set of bytes.
- * @s: The string to be searched.
- * @accept: The set of bytes to be searched for.
+ * _strpbrk - Locates the first occurrence of any byte in accept in s.
  *
- * Return: If a set is matched - a pointer to the matched byte.
- * If no set is matched - NULL.
+ * @s: Pointer to the input string to search.
+ * @accept: Pointer to the string containing the bytes to search for.
+ *
+ * Return: Pointer to the first occurrence of a matching byte in s, or NULL if
+ *         no matching byte is found.
+ *
+ * Description: The function searches the string s for the first occurrence of
+ * any byte in the string accept. returns a pointer to the matching byte in s,
+ * or NULL if no matching byte found. The standard library provides a similar
+ * function: strpbrk. Run 'man strpbrk' for more information.
  */
 char *_strpbrk(char *s, char *accept)
 {
-	while (*s)
-	{
-		int ind = 0;
+	if (s == NULL || accept == NULL)
+		return (NULL);
 
-		while (accept[ind])
+	char *s_ptr = s;
+	char *accept_ptr;
+
+	for (i = 0; s_ptr[i] != '\0'; i++)
+	{
+		accept_ptr = accept;
+		for (j = 0; accept_ptr[j] != '\0'; j++)
 		{
-			if (*s == accept[ind])
-				return (s);
-			ind++;
+			if (*s_ptr == *accept_ptr)
+				return (*s_ptr);
 		}
-		s++;
 	}
-	return ('\0');
+	return (NULL)
+
 }
+
