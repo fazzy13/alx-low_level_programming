ba8ddbd17aa00e4bffe4e37b78df619b9d6abe51
fazzy13 <kaluifeanyi003@gmail.com>
2023-04-14 18:43:08 +0100

    implementation for _puts_recursion function

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+/**
+ * _puts_recursion - Prints a string followed by a new line.
+ *
+ * @s: Pointer to a null-terminated string to be printed.
+ *
+ * Description: This function recursively prints a string @s followed by a new line
+ * to the standard output. It starts printing the string from the first character
+ * and continues until the null terminator is encountered.
+ *
+ * Return: None (void).
+ *
+ * Usage: _puts_recursion("Hello, world!");
+ *        _puts_recursion("Welcome to Recursion!");
+ */
+void _puts_recursion(char *s)
+{
+	if (s == '\0' || s == NULL)
+		return;
+	else
+		_putchar(*s);
+	_puts_recursion(s + 1);
+}
