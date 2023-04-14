1731944c182d812967118e935d79e7513e8c5846
fazzy13 <kaluifeanyi003@gmail.com>
2023-04-14 18:44:21 +0100

    implementation for _puts_recursion function

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * _puts_recursion - Prints a string followed by a new line.
  *
@@ -14,7 +16,7 @@
  */
 void _puts_recursion(char *s)
 {
-	if (s == '\0' || s == NULL)
+	if (*s == '\0' || s == NULL)
 		return;
 	else
 		_putchar(*s);
