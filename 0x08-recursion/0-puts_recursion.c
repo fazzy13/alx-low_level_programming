10ffc869ebadcd60d544597a1084f02ed0834bab
fazzy13 <kaluifeanyi003@gmail.com>
2023-04-14 18:57:32 +0100

    implementation for _puts_recursion function

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -18,8 +18,12 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0' || s == NULL)
+	{
 		return;
-
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
