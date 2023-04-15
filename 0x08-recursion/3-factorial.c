e0b1f134c6031fb27245badd8d21d32cf4ed21b3
fazzy13 <kaluifeanyi003@gmail.com>
2023-04-15 15:23:39 +0100

    Add factorial function

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,15 +6,13 @@
  */
 int factorial(int n)
 {
-    /* Base case: factorial of 0 is 1 */
-    if (n == 0)
-        return 1;
 
-    /* Error case: factorial of negative numbers is not defined */
-    if (n < 0)
-        return -1;
+	if (n == 0)
+		return 1;
 
-    /* Recursive case: multiply n with factorial of (n-1) */
-    return n * factorial(n - 1);
+	if (n < 0)
+		return -1;
+
+	return n * factorial(n - 1);
 }
 
