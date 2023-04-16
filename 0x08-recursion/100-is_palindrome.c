c61dbb24d11e40c324383464ad87d80d89b7e4e9
fazzy13 <kaluifeanyi003@gmail.com>
2023-04-16 17:47:58 +0100

    check if a string is a palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,8 +10,8 @@ int is_palindrome(char *s)
 {
 	int check_palindrome(char *s, int start, int end);
 	
-    int len = strlen(s);
-    return check_palindrome(s, 0, len - 1);
+	int len = strlen(s);
+	return (check_palindrome(s, 0, len - 1));
 }
 
 /**
@@ -24,12 +24,12 @@ int is_palindrome(char *s)
  */
 int check_palindrome(char *s, int start, int end)
 {
-    if (start >= end)
-        return 1;
+	if (start >= end)
+		return (1);
 
-    if (s[start] != s[end])
-        return 0;
+	if (s[start] != s[end])
+		return (0);
 
-    return check_palindrome(s, start + 1, end - 1);
+	return (check_palindrome(s, start + 1, end - 1));
 }
 
