f7c26496131e9931489d74a663ddbaae24349836
fazzy13 <kaluifeanyi003@gmail.com>
2023-04-20 17:41:38 +0100

    function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,44 +1,43 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * str_concat - concatenates
- * @s1: string 1
- * @s2: string 2
- * Return: pointer to concatenated string
+ * concat_strings - Concatenate two strings
+ * @s1: Pointer to the first input string
+ * @s2: Pointer to the second input string
+ *
+ * Return: Pointer to the concatenated string, or NULL on failure
  */
-char *str_concat(char *s1, char *s2)
+char *concat_strings(char *s1, char *s2)
 {
 	char *concat;
-	int len1 = 0, len2 = 0, i = 0, j = 0;
+	size_t len1, len2;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	while (*(s1 + i))
-		len1++, i++;
-	while (*(s2 + j))
-		len2++, j++;
-	len2++;
-
-	concat = malloc(sizeof(char) * (len1 + len2));
-
-	if (concat == NULL)
+	/* Check if either input string is NULL */
+	if (s1 == NULL || s2 == NULL)
+	{
 		return (NULL);
+	}
 
-	i = 0, j = 0;
-	while (i < len1)
+	/* Get the lengths of the input strings */
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	/* Allocate memory for the concatenated string */
+	concat = (char *)malloc(len1 + len2 + 1);
+
+	/* Check if memory allocation was successful */
+	if (concat == NULL)
 	{
-		*(concat + i) = *(s1 + i);
-		i++;
+		return (NULL);
 	}
-	while (j < len2)
-	{
-		*(concat + i) = *(s2 + j);
-		i++, j++;
-	}
+
+	/* Copy the first input string to the concatenated string */
+	strcpy(concat, s1);
+
+	/* Concatenate the second input string to the concatenated string */
+	strcat(concat, s2);
 
 	return (concat);
 }
