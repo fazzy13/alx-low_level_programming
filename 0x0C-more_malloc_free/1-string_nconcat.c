3698d7d5348cf81bddc93fde9669fda5f27fff1d
fazzy13 <kaluifeanyi003@gmail.com>
2023-04-23 07:36:38 +0100

    function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,44 +1,52 @@
-#include "main.h"
 #include <stdlib.h>
 
 /**
- * string_nconcat - Concatenates two
- * @s1: The first string.
- * @s2: The second string.
- * @n: The maximum number of 
+ * string_nconcat - Concatenate two strings
+ * @s1: Pointer to the first string
+ * @s2: Pointer to the second string
+ * @n: Number of bytes from s2 to concatenate
  *
- * Return: If the function fails - NULL.
- * Otherwise - a pointer to the
+ * Return: Pointer to newly allocated memory block containing the concatenated
+ * string, or NULL on failure.
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *concat;
-	unsigned int len = n, index;
+	unsigned int len1 = 0, len2 = 0;
+	char *concatenated = NULL;
+	unsigned int i, j;
 
+	/* Treat NULL as an empty string */
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
 
-	for (index = 0; s1[index]; index++)
-		len++;
+	/* Calculate lengths of s1 and s2 */
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
 
-	concat = malloc(sizeof(char) * (len + 1));
+	/* Use entire string s2 if n is greater or equal to its length */
+	if (n >= len2)
+		n = len2;
 
-	if (concat == NULL)
+	/* Allocate memory for concatenated string */
+	concatenated = (char *)malloc((len1 + n + 1) * sizeof(char));
+	if (concatenated == NULL)
 		return (NULL);
 
-	len = 0;
+	/* Copy s1 to concatenated string */
+	for (i = 0; i < len1; i++)
+		concatenated[i] = s1[i];
 
-	for (index = 0; s1[index]; index++)
-		concat[len++] = s1[index];
+	/* Copy first n bytes of s2 to concatenated string */
+	for (j = 0; j < n; j++, i++)
+		concatenated[i] = s2[j];
 
-	for (index = 0; s2[index] && index < n; index++)
-		concat[len++] = s2[index];
+	/* Null-terminate the concatenated string */
+	concatenated[i] = '\0';
 
-	concat[len] = '\0';
-
-	return (concat);
+	return (concatenated);
 }
+
