fe785f22ba0aec179a6a982a71dace7b37c1439b
fazzy13 <kaluifeanyi003@gmail.com>
2023-06-09 16:51:26 +0100

    script called create_static_lib.sh that creates a static library called liball.a

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+
+# Compile all .c files in the current directory into object files
 gcc -c *.c
+
+# Create the static library liball.a from the object files
 ar rc liball.a *.o
+
+# Index the library
+ranlib liball.a
+
+# Clean up the object files
+rm *.o
+
