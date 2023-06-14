1dc2de698049fc58aaace2d07e4a968f954b0dad
fazzy13 <kaluifeanyi003@gmail.com>
2023-06-14 22:56:01 +0100

    concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,34 +10,34 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    char *concat;
-    size_t len1 = 0, len2 = 0;
+	char *concat;
+	size_t len1 = 0, len2 = 0;
 
-    /* Check if either input string is NULL */
-    if (s1 != NULL)
-        len1 = strlen(s1);
-    if (s2 != NULL)
-        len2 = strlen(s2);
+	/* Check if either input string is NULL */
+	if (s1 != NULL)
+		len1 = strlen(s1);
+	if (s2 != NULL)
+		len2 = strlen(s2);
 
-    /* Allocate memory for the concatenated string */
-    concat = (char *)malloc(len1 + len2 + 1);
+	/* Allocate memory for the concatenated string */
+	concat = (char *)malloc(len1 + len2 + 1);
 
-    /* Check if memory allocation was successful */
-    if (concat == NULL)
-    {
-        return NULL;
-    }
+	/* Check if memory allocation was successful */
+	if (concat == NULL)
+	{
+		return NULL;
+	}
 
-    /* Copy the first input string to the concatenated string */
-    if (s1 != NULL)
-        strcpy(concat, s1);
-    else
-        concat[0] = '\0';
+	/* Copy the first input string to the concatenated string */
+	if (s1 != NULL)
+		strcpy(concat, s1);
+	else
+		concat[0] = '\0';
 
-    /* Concatenate the second input string to the concatenated string */
-    if (s2 != NULL)
-        strcat(concat, s2);
+	/* Concatenate the second input string to the concatenated string */
+	if (s2 != NULL)
+		strcat(concat, s2);
 
-    return concat;
+	return concat;
 }
 
