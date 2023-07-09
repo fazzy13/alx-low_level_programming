6e2a39ee5b4c1f704be4f3736e5946c6ecf1d3be
fazzy13 <kaluifeanyi003@gmail.com>
2023-07-09 05:57:34 +0100

    sets the value of a bit to 0 at a given index.

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,14 +9,16 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int mask;
+
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
+	mask = 1UL << index;
 	/* Creates a mask with a 1 in the desired bit position */
-	unsigned long int mask = 1UL << index;
 
-	/* Inverts the mask and performs bitwise AND to set the bit to 0 */
 	*n &= ~mask;
+	/* Inverts the mask and performs bitwise AND to set the bit to 0 */
 
 	return (1);
 }
