402ccd3d934fe33648fab6b79bafb28a5fb16883
fazzy13 <kaluifeanyi003@gmail.com>
2023-07-11 20:28:22 +0100

    FILE i/O:  reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,36 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output
+ * read_textfile - Reads a text file and prints it to the POSIX standard output.
  * @filename: The name of the file to read.
  * @letters: The number of letters to read and print.
  *
- * Return: The actual number of letters read and printed.
+ * Return: The actual number of letters read and printed, or 0 on failure.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *file;
+	int file_descriptor;
 	ssize_t bytes_read, bytes_written;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	file = fopen(filename, "r");
-	if (file == NULL)
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
 		return (0);
 
 	buffer = allocate_buffer(letters);
 	if (buffer == NULL)
 	{
-		fclose(file);
+		close(file_descriptor);
 		return (0);
 	}
 
-	bytes_read = read_from_file(buffer, letters, file);
+	bytes_read = read_from_file(file_descriptor, buffer, letters);
 	if (bytes_read == -1)
 	{
-		fclose(file);
+		close(file_descriptor);
 		free_buffer(buffer);
 		return (0);
 	}
@@ -38,81 +38,57 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	bytes_written = write_to_stdout(buffer, bytes_read);
 	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
-		fclose(file);
+		close(file_descriptor);
 		free_buffer(buffer);
 		return (0);
 	}
 
-	fclose(file);
+	close(file_descriptor);
 	free_buffer(buffer);
 	return (bytes_written);
 }
 
 /**
- * allocate_buffer - Allocates memory for the buffer.
- * @letters: The number of letters to read.
+ * allocate_buffer - Allocates memory for a buffer.
+ * @size: The size of the buffer to allocate.
  *
  * Return: A pointer to the allocated buffer, or NULL on failure.
  */
-char *allocate_buffer(size_t letters)
+char *allocate_buffer(size_t size)
 {
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * (letters + 1));
-	if (buffer == NULL)
-	{
-		fprintf(stderr, "Error: Failed to allocate buffer\n");
-		return (NULL);
-	}
-
-	return (buffer);
+	char *buffer = malloc(sizeof(char) * (size + 1));
+	return buffer;
 }
 
 /**
- * read_from_file - Reads from the file into the buffer.
+ * read_from_file - Reads data from a file.
+ * @file_descriptor: The file descriptor of the file to read from.
  * @buffer: The buffer to store the read data.
- * @letters: The number of letters to read.
- * @file: The file to read from.
+ * @size: The number of bytes to read.
  *
  * Return: The number of bytes read, or -1 on failure.
  */
-ssize_t read_from_file(char *buffer, size_t letters, FILE *file)
+ssize_t read_from_file(int file_descriptor, char *buffer, size_t size)
 {
-	ssize_t bytes_read;
-
-	bytes_read = fread(buffer, sizeof(char), letters, file);
-	if (bytes_read == -1)
-	{
-		fprintf(stderr, "Error: Failed to read from file\n");
-		return (-1);
-	}
-
-	return (bytes_read);
+	ssize_t bytes_read = read(file_descriptor, buffer, size);
+	return bytes_read;
 }
 
 /**
- * write_to_stdout - Writes the buffer to the standard output.
- * @buffer: The buffer to write.
- * @bytes: The number of bytes to write.
+ * write_to_stdout - Writes data to the POSIX standard output.
+ * @buffer: The buffer containing the data to write.
+ * @size: The number of bytes to write.
  *
  * Return: The number of bytes written, or -1 on failure.
  */
-ssize_t write_to_stdout(const char *buffer, size_t bytes)
+ssize_t write_to_stdout(const char *buffer, size_t size)
 {
-	ssize_t bytes_written;
-
-	bytes_written = fwrite(buffer, sizeof(char), bytes, stdout);
-	if (bytes_written == -1)
-	{
-		fprintf(stderr, "Error: Failed to write to standard output\n");
-		return (-1);
-	}
-
-	return (bytes_written);
+	ssize_t bytes_written = write(STDOUT_FILENO, buffer, size);
+	return bytes_written;
 }
 
 /**
- * free_buffer - Frees the memory allocated for the buffer.
+ * free_buffer - Frees the memory allocated for a buffer.
  * @buffer: The buffer to free.
  */
 void free_buffer(char *buffer)
