1c636ceb47a678b812c7f855c1506337c6c691cd
fazzy13 <kaluifeanyi003@gmail.com>
2023-07-11 21:26:38 +0100

    FILE i/O:  function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file with the given content.
+ * @filename: The name of the file to create.
+ * @text_content: The content to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_descriptor, write_result;
+	ssize_t text_length = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file_descriptor == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[text_length])
+			text_length++;
+
+		write_result = write(file_descriptor, text_content, text_length);
+		if (write_result == -1)
+		{
+			close(file_descriptor);
+			return (-1);
+		}
+	}
+
+	close(file_descriptor);
+	return (1);
+}
+
