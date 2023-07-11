ba16fa7ca71263cae3335e575274f8613cfd105d
fazzy13 <kaluifeanyi003@gmail.com>
2023-07-11 20:27:57 +0100

    FILE i/O:  update more prototypes

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,16 +4,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 /* Function prototypes */
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-char *allocate_buffer(size_t letters);
-ssize_t read_from_file(char *buffer, size_t letters, FILE *file);
-ssize_t write_to_stdout(const char *buffer, size_t bytes);
+
+char *allocate_buffer(size_t size);
+ssize_t read_from_file(int file_descriptor, char *buffer, size_t size);
+ssize_t write_to_stdout(const char *buffer, size_t size);
 void free_buffer(char *buffer);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
 
 #endif /* MAIN_H */
 
