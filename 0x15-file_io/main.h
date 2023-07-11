e23a4c0ccd1f01f45d272e5eca119790a244317d
fazzy13 <kaluifeanyi003@gmail.com>
2023-07-11 19:05:42 +0100

    FILE i/O:  update more prototypes

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,6 +10,10 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+char *allocate_buffer(size_t letters);
+ssize_t read_from_file(char *buffer, size_t letters, FILE *file);
+ssize_t write_to_stdout(const char *buffer, size_t bytes);
+void free_buffer(char *buffer);
 
 #endif /* MAIN_H */
 
